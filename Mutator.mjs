2aea162dca077c5c10b1e674adf1d4bc67243a31
Burak Arslan <burak.arslan@arskom.com.tr>
2025-10-10 15:11:19 +0300

    Chapter 1 implementation

--- Mutator.mjs
+++ Mutator.mjs
@@ -8,6 +8,7 @@ import "soba://computer/R1";
 
 import {
     DeleteRow,
+    Message,
 } from "https://sobamail.com/module/base/v1?sha224=LbrFSXuQxm2gn4-FaglNXRQbcv7kAz9Zew-p1A";
 
 export default class Responder {
@@ -25,8 +26,31 @@ export default class Responder {
     }
 
     process(message, metadata) {
-        // TODO: Implement the app logic
+        // If you prefer
+        soba.log.info("Hello World!");
+
+        // FYI: Show incoming data
+        soba.log.info(`metadata: ${JSON.stringify(metadata, null, 2)}`);
+        soba.log.info(` message: ${JSON.stringify(message, null, 2)}`);
+
+        let reply = new Message();
+
+        reply.from = [ {address : soba.app.account()} ];
+        reply.to = [ {name : message.fromName, address : message.fromAddress} ];
+        reply.subject = "Hello World! I'm on Vacation!";
+
+        reply.bodyText = "I am on vacation and will not be able to respond to" +
+                " your message in a timely manner." +
+                "\nPlease contact me some other time.";
+
+        reply.bodyHtml = "<p>" +
+                "I am on vacation and will not be able to respond to" +
+                " your message in a timely manner.<br>" +
+                "\nPlease contact me some other time." +
+                "</p>";
+
+        soba.log.info(`Sent vacation response to "${reply.to[0].name} <${reply.to[0].address}>"`);
+
+        soba.mail.send(reply);
     }
 }
-
-
